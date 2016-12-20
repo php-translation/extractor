787b8237dfb165d7dcfa3edc75904734f2a0da0e
Tobias Nyholm <tobias.nyholm@gmail.com>
2016-12-20 21:08:44 +0100

    Added placeholder extractor

---
+++ tests/Resources/Php/Symfony/PlaceholderFormType.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Translation\Extractor\Tests\Resources\Php\Symfony;
+
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+class PlaceholderFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('field_with_attr_placeholder', 'text', array(
+                'label' => 'field.with.placeholder',
+                'attr' => array('placeholder' => 'form.placeholder.text')
+            ))
+            ->add('field_without_label_with_attr_placeholder', 'text', array(
+                'label' => false,
+                'attr' => array('placeholder' => 'form.placeholder.text.but.no.label')
+            ))
+            ->add('field_placeholder', 'choice', array(
+                'placeholder' => 'form.choice_placeholder'
+            ))
+        ;
+    }
+}
