675f11a7fe977541f9b58e35b98084179055a8a0
Tobias Nyholm <tobias.nyholm@gmail.com>
2016-12-21 18:19:02 +0100

    Added placeholder extractor (#16)
    * Added placeholder extractor
    
    * cs

---
+++ src/Visitor/Php/Symfony/FormTypePlaceholder.php
@@ -0,0 +1,58 @@
+<?php
+
+/*
+ * This file is part of the PHP Translation package.
+ *
+ * (c) PHP Translation team <tobias.nyholm@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Translation\Extractor\Visitor\Php\Symfony;
+
+use PhpParser\Node;
+use PhpParser\Node\Stmt;
+use PhpParser\NodeTraverser;
+use PhpParser\NodeVisitor;
+use Translation\Extractor\Model\SourceLocation;
+use Translation\Extractor\Visitor\Php\BasePHPVisitor;
+
+class FormTypePlaceholder extends BasePHPVisitor implements NodeVisitor
+{
+    public function enterNode(Node $node)
+    {
+        // only Traverse *Type
+        if ($node instanceof Stmt\Class_) {
+            if (substr($node->name, -4) !== 'Type') {
+                return NodeTraverser::DONT_TRAVERSE_CHILDREN;
+            }
+        }
+
+        if (!$node instanceof Node\Expr\Array_) {
+            return;
+        }
+
+        foreach ($node->items as $item) {
+            if (!$item->key instanceof Node\Scalar\String_) {
+                continue;
+            }
+
+            if ($item->key->value === 'placeholder' && $item->value instanceof Node\Scalar\String_) {
+                $this->collection->addLocation(new SourceLocation($item->value->value, $this->getAbsoluteFilePath(), $item->value->getAttribute('startLine')));
+            }
+        }
+    }
+
+    public function leaveNode(Node $node)
+    {
+    }
+
+    public function beforeTraverse(array $nodes)
+    {
+    }
+
+    public function afterTraverse(array $nodes)
+    {
+    }
+}
---
+++ tests/Functional/Visitor/Php/Symfony/FormTypePlaceholderTest.php
@@ -0,0 +1,29 @@
+<?php
+
+/*
+ * This file is part of the PHP Translation package.
+ *
+ * (c) PHP Translation team <tobias.nyholm@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Translation\Extractor\Tests\Resources\Php\Symfony;
+
+use Translation\Extractor\Tests\Functional\Visitor\Php\BasePHPVisitorTest;
+use Translation\Extractor\Tests\Resources;
+use Translation\Extractor\Visitor\Php\Symfony\FormTypePlaceholder;
+
+class FormTypePlaceholderTest extends BasePHPVisitorTest
+{
+    public function testExtract()
+    {
+        $collection = $this->getSourceLocations(new FormTypePlaceholder(), Resources\Php\Symfony\PlaceholderFormType::class);
+
+        $this->assertCount(3, $collection);
+        $this->assertEquals('form.placeholder.text', $collection->get(0)->getMessage());
+        $this->assertEquals('form.placeholder.text.but.no.label', $collection->get(1)->getMessage());
+        $this->assertEquals('form.choice_placeholder', $collection->get(2)->getMessage());
+    }
+}
