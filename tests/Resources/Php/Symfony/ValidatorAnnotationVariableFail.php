2573a28e2e859cca72945faf469942f4fa24513a
Piotr Nowak <piotr@silqcode.com>
2018-07-04 09:24:51 +0200

    removal of test files related to ValidatorAnnotation issues

--- tests/Resources/Php/Symfony/ValidatorAnnotationVariableFail.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace Translation\Extractor\Tests\Resources\Php\Symfony;
-
-class ValidatorAnnotationVariableFail
-{
-    protected $someVar = /** @Translate */'some_var';
-}
--- tests/Resources/Php/Symfony/ValidatorAnnotationVariablePass.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-namespace Translation\Extractor\Tests\Resources\Php\Symfony;
-
-class ValidatorAnnotationVariablePass
-{
-    const SOME_CONST = /** @Translate */'some_const';
-}
