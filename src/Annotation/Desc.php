f856c2b6251cd91e7436df8f3160e272a2608dec
mgiraud <marc@mac.home>
2026-03-16 12:26:16 +0100

    More explicit deprecation message

--- src/Annotation/Desc.php
+++ src/Annotation/Desc.php
@@ -11,7 +11,7 @@
 
 namespace Translation\Extractor\Annotation;
 
-trigger_deprecation('php-translation/extractor', '2.3', 'The "%s" class is deprecated with no replacement, it is now considered as a PHPDoc tag.', Desc::class);
+trigger_deprecation('php-translation/extractor', '2.3', 'The "%s" class is deprecated and will be removed soon. It is now considered as a PHPDoc tag.', Desc::class);
 /**
  * @deprecated since 2.3, this class is not used anymore. @Desc is now considered as a PHPDoc tag.
  *
--- src/Annotation/Ignore.php
+++ src/Annotation/Ignore.php
@@ -11,7 +11,7 @@
 
 namespace Translation\Extractor\Annotation;
 
-trigger_deprecation('php-translation/extractor', '2.3', 'The "%s" class is deprecated with no replacement, it is now considered as a PHPDoc tag.', Ignore::class);
+trigger_deprecation('php-translation/extractor', '2.3', 'The "%s" class is deprecated and will be removed soon. It is now considered as a PHPDoc tag.', Ignore::class);
 /**
  * @deprecated since 2.3, this class is not used anymore. @Ignore is now considered as a PHPDoc tag.
  *
